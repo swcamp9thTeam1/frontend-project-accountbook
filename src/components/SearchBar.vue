f2e9601feceef8e6a14a7f699e2f8972c0f86419
yanghyeonjin <guswls7081@gmail.com>
2024-09-24 18:41:23 +0900

    SearchBar css 수정
    - focus-visible 상태일 때 outline 제거

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="search-bar">
-        <input type="text" class="search-bar-input">
         <img :src="SearchIcon" alt="">
+        <input type="text" class="search-bar-input">
     </div>
 </template>
 
@@ -12,33 +12,35 @@ import SearchIcon from "@/assets/icons/search.svg";
 
 <style scoped>
 .search-bar {
-    width: 100%;
-    height: 77px;
     border-radius: 10px;
     border: 1px solid #B1B1D2;
     background: #fff;
     box-shadow: 5px 5px 9px 0px rgba(0, 0, 0, 0.25);
     position: relative;
     overflow: hidden;
+    display: flex;
+    align-items: center;
+    padding: 20px;
 
     img {
-        position: absolute;
-        left: 20px;
-        top: 50%;
-        transform: translateY(-50%);
+        flex-shrink: 0;
     }
 }
 .search-bar-input {
     font-size: 24px;
     font-weight: 700;
     border: none;
-    width: 100%;
     height: 100%;
-    padding: 0 16px 0 70px;
+    flex-grow: 1;
+    margin-left: 20px;
     
-    ::placeholder {
+    &::placeholder {
         color: rgba(0, 0, 0, 0.37);
         font-weight: 400;
+    }
+
+    &:focus-visible {
+        outline: none;
     }
 }
 </style>
