094e1e4a2d4608583f0836cec0b7a371334d5adc
yanghyeonjin <guswls7081@gmail.com>
2024-09-24 15:50:25 +0900

    Vue 프로젝트 셋팅

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <select name="" id="" v-model="username">
+    <option value="채연">채연 컴포넌트</option>
+    <option value="시우">시우 컴포넌트</option>
+    <option value="현진">현진 컴포넌트</option>
+    <option value="호정">호정 컴포넌트</option>
+    <option value="다민">다민 컴포넌트</option>
+    <option value="경희">경희 컴포넌트</option>
+  </select>
+
+  <div v-if="username === '채연'">
+    <!-- 채연 컴포넌트 -->
+  </div>
+
+  <div v-else-if="username === '시우'">
+    <!-- 시우 컴포넌트 -->
+  </div>
+
+  <div v-else-if="username === '현진'">
+    <!-- 현진 컴포넌트 -->
+  </div>
+
+  <div v-else-if="username === '호정'">
+    <!-- 호정 컴포넌트 -->
+  </div>
+
+  <div v-else-if="username === '다민'">
+    <!-- 다민 컴포넌트 -->
+  </div>
+
+  <div v-else>
+    <!-- 경희 컴포넌트 -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const username = ref("");
+</script>
+
+<style scoped>
+
+</style>
