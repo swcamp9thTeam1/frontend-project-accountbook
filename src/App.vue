9f0f7e3653220117061f8efcefe439cdb2d64264
yanghyeonjin <guswls7081@gmail.com>
2024-09-25 12:08:13 +0900

    앱 푸터 파일 추가

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
   <main>
     <RouterView />
   </main>
+  <AppFooter />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 
 import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
 </script>
 
 <style scoped>
