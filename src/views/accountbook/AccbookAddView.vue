b0739d8ce23c627c72bd18c93498f54527279170
Rohdamin <rohdamin3@gmail.com>
2024-09-26 16:58:01 +0900

    FEATURE: 가계부 추가(생성) 페이지 작성

--- src/views/accountbook/AccbookAddView.vue
+++ src/views/accountbook/AccbookAddView.vue
@@ -1,13 +1,22 @@
 <template>
-    <h1>
-        add
-    </h1>
+  <div class="parent-container">
+    <AccountBookInsert/>
+  </div>
+
 </template>
 
 <script setup>
 
+import AccountBookInsert from "@/components/accbook/AccountBookInsert.vue";
 </script>
 
 <style scoped>
+.parent-container {
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
 </style>
