a50e8007bf74db062c0b2e7713a8b9faa0167d94
Rohdamin <rohdamin3@gmail.com>
2024-09-29 23:20:02 +0900

    FEATURE: 캘린더 css 변경
    - 캘린더에 월 예산에 따른 평가 표시 (Good일 경우 파랑, Bad일 경우 빨강으로 표시)
    - 캘린더 상단 남은 예산 메세지, 통계 보러가기 버튼 추가
    - 캘린더에서 월 클릭 -> 가계부 메인 페이지(월별 전체 조회)로 이동
    - 캘린더 월 표시, 요일 표시 변경

--- src/components/calendar/CustomCalendarView.vue
+++ src/components/calendar/CustomCalendarView.vue
@@ -1,30 +1,42 @@
 <template>
-    <div>
-        <CalendarView 
-            :show-date="showDate" 
-            :starting-day-of-week="0"
-            :items="state.items"
-            @clickDate="onClickDate"
-            class="theme-default holiday-us-traditional holiday-us-official">
-            <template #header="{ headerProps }">
-                <CustomCalendarViewHeader 
-                    :previous-period-label="themeOptions.previousPeriodLabel" 
-                    :header-props="headerProps"
-                    :next-period-label="themeOptions.nextPeriodLabel"
-                    @input="setShowDate" 
-                    />
-            </template>
-        </CalendarView>
-    </div>
-    <div>
-    {{ state.message }}
-</div>
+  <div>
+    <CalendarView
+        :show-date="showDate"
+        :starting-day-of-week="0"
+        :items="state.items"
+        :weekday-name-format="'short'"
+        :locale="'en-US'"
+        @clickDate="handleClickDate"
+        :current-period-label="themeOptions.currentPeriodLabel"
+        :class="`theme-${state.theme}`"
+        class="theme-default holiday-us-traditional holiday-us-official">
 
+      <!-- 헤더 슬롯 -->
+      <template #header="{ headerProps }">
+        <CustomCalendarViewHeader
+            :previous-period-label="themeOptions.previousPeriodLabel"
+            :header-props="headerProps"
+            :next-period-label="themeOptions.nextPeriodLabel"
+            @input="setShowDate"
+        />
+      </template>
+
+      <!-- 날짜 슬롯 -->
+      <template #day-content="{ day }">
+        <div :class="[getEvaluationClass(day)]">
+          {{ day.getDate()}}
+        </div>
+      </template>
+    </CalendarView>
+  </div>
+  <div>
+    {{ state.message }}
+  </div>
 </template>
 
 <script setup>
-import { CalendarView } from 'vue-simple-calendar';
-import { ref, computed, reactive, onMounted, defineEmits } from 'vue';
+import {CalendarView} from 'vue-simple-calendar';
+import {ref, computed, reactive, onMounted, defineEmits, toRaw } from 'vue';
 
 import 'vue-simple-calendar/dist/style.css';
 import 'vue-simple-calendar/dist/css/default.css';
@@ -34,93 +46,126 @@ import router from "@/router/router.js";
 
 const showDate = ref(new Date());
 const emit = defineEmits(['dateClicked']);
+const accbookdata = ref([]);
+
 const setShowDate = (d) => {
-    showDate.value = d;
+  showDate.value = d;
 };
 
 const state = reactive({
-    theme: "gcal",
-    items: []  // items의 초기값을 빈 배열로 설정
+  theme: "gcal",
+  items: []  // items의 초기값을 빈 배열로 설정
 });
 
 const themeOptions = computed(() => state.theme === "gcal"
     ? {
-        top: "2.6em",
-        height: "2.1em",
-        border: "0px",
-        previousYearLabel: "\uE5CB\uE5CB",
-        previousPeriodLabel: "\uE5CB",
-        nextPeriodLabel: "\uE5CC",
-        nextYearLabel: "\uE5CC\uE5CC",
-        currentPeriodLabel: "Today",
+      top: "2.6em",
+      height: "2.1em",
+      border: "0px",
+      previousYearLabel: "\uE5CB\uE5CB",
+      previousPeriodLabel: "\uE5CB",
+      nextPeriodLabel: "\uE5CC",
+      nextYearLabel: "\uE5CC\uE5CC",
+      currentPeriodLabel: "Today",
     }
     : {
-        top: "1.4em",
-        height: "1.4em",
-        border: "2px",
-        previousYearLabel: "<<",
-        previousPeriodLabel: "<",
-        nextPeriodLabel: ">",
-        nextYearLabel: ">>",
-        currentPeriodLabel: "",
+      top: "1.4em",
+      height: "1.4em",
+      border: "2px",
+      previousYearLabel: "<<",
+      previousPeriodLabel: "<",
+      nextPeriodLabel: ">",
+      nextYearLabel: ">>",
+      currentPeriodLabel: "",
     }
 );
 
+// 수입/지출 아이템을 생성하는 메서드
 const createCalendarItems = (accbookdata) => {
-    var items = [];
-    accbookdata.forEach((entry) => {
-        const startDay = new Date(entry.dailyDate);
-        const endDay = new Date(entry.dailyDate);
+  var items = [];
+  accbookdata.forEach((entry) => {
+    const startDay = new Date(entry.dailyDate);
+    const endDay = new Date(entry.dailyDate);
 
-        // totalIAmount가 0이 아니면 수입 아이템 생성
-        if (entry.totalIAmount !== 0) {
-            const inItem = {
-                id: `${entry.dailyDate}-I`,
-                title: `+${entry.totalIAmount.toLocaleString()}`,
-                startDate: startDay,
-                endDate: endDay,
-                classes: ['item-in', 'item-top'],
-            };
-            items.push(inItem);
-        }
-        // totalOAmount가 0이 아니면 지출 아이템 생성
-        if (entry.totalOAmount !== 0) {
-            const outItem = {
-                id: `${entry.dailyDate}-O`,
-                title: `-${entry.totalOAmount.toLocaleString()}`,
-                startDate: startDay,
-                endDate: endDay,
-                classes: ['item-out', 'item-top'],
-            };
-            items.push(outItem);
-        }
-    });
+    // totalIAmount가 0이 아니면 수입 아이템 생성
+    if (entry.totalIAmount !== 0) {
+      const inItem = {
+        id: `${entry.dailyDate}-I`,
+        title: `+${entry.totalIAmount.toLocaleString()}`,
+        startDate: startDay,
+        endDate: endDay,
+        classes: ['item-in', 'item-top'],
+      };
+      items.push(inItem);
+    }
+    // totalOAmount가 0이 아니면 지출 아이템 생성
+    if (entry.totalOAmount !== 0) {
+      const outItem = {
+        id: `${entry.dailyDate}-O`,
+        title: `-${entry.totalOAmount.toLocaleString()}`,
+        startDate: startDay,
+        endDate: endDay,
+        classes: ['item-out', 'item-top'],
+      };
+      items.push(outItem);
+    }
+  });
 
-    return items;
+  return items;
 };
 
-// 초기 데이터 생성
-const accbookdata = ref([]);
+// 평가에 따른 클래스 이름을 반환하는 메서드
+const getEvaluationClass = (date) => {
+  const todayString = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+  const entry = accbookdata.value.find(item => item.dailyDate === todayString);
+
+  if (!entry) {
+    return 'cv-day-no-eval'; // entry가 null인 경우
+  }
+
+  const rawEntry = toRaw(entry);
+
+  // evaluation이 null인 경우 'cv-day-no-eval' 클래스를 반환
+  if (rawEntry.evaluation === null || rawEntry.evaluation === undefined) {
+    return 'cv-day-no-eval';
+  }
+
+  return rawEntry.evaluation === "Good" ? 'cv-day-good' : 'cv-day-bad';
+}
+
+// 서버에서 평가 데이터를 가져오는 메서드
+const fetchMonthlyEvalData = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/monthly-eval');
+    if (!response.ok) {
+      throw new Error('데이터 가져오기 실패');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('데이터를 가져오는 중 오류 발생:', error);
+    return null;
+  }
+};
+
+// accbookdata와 state.items를 업데이트하는 메서드
+const updateCalendarItems = (data) => {
+  if (data) {
+    // accbookdata 업데이트
+    accbookdata.value = data;
+
+    // result 배열을 사용해 아이템 생성
+    state.items = createCalendarItems(data);
+  }
+};
 
 onMounted(async () => {
-    try {
-        // fetch로 json 데이터 가져오기
-        const response = await fetch('http://localhost:8080/monthly-eval');
-        const data = await response.json(); 
-
-        // accbookdata에 가져온 데이터 저장
-        accbookdata.value = data;
-
-        // result 배열을 사용해 아이템 생성
-        state.items = createCalendarItems(data); 
-    } catch (error) {
-        console.error('데이터를 가져오는 중 오류 발생:', error);
-    }
+  const data = await fetchMonthlyEvalData(); // 데이터 가져오기
+  updateCalendarItems(data); // 캘린더 아이템 업데이트
 });
 
-const onClickDate = (day) => {
+const handleClickDate = (day) => {
   const todayString = `${day.getFullYear()}-${(day.getMonth() + 1).toString().padStart(2, '0')}-${day.getDate().toString().padStart(2, '0')}`;
-  // console.log("You clicked:", todayString);
 
   // 날짜 클릭 시 부모 컴포넌트로 이벤트 전달 (부모 컴포넌트에서 라우팅 처리)
   emit('dateClicked', todayString);
@@ -128,31 +173,39 @@ const onClickDate = (day) => {
 
 </script>
 
-<!-- <style scoped> -->
 <style>
 
 .cv-wrapper {
-	width: 607px;
-	height: 496px;
-	left: 88px;
-	top: 308px;
-	background: #F9F9FF;
-	border-radius: 30px;
+  width: 607px;
+  height: 496px;
+  left: 88px;
+  top: 308px;
+  background: #F9F9FF;
+  border-radius: 30px;
   font-family: 'Noto Sans KR';
+  box-shadow: 0px 0px 7px 1px #DFDFF5;
+  border: none;
+  cursor: pointer;
+  margin: 10px;
+
+  padding-top: 20px;
+  padding-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
 }
 
 
 /* item margin용 css */
 .item-top {
-    margin-top: 10px; /* 최상위 아이템에만 주는 margin 값 */
+  margin-top: 20px; /* 최상위 아이템에만 주는 margin 값 */
 }
 
 .item-in {
-    color: #0A88BE;
+  color: #0A88BE;
 }
 
 .item-out {
-    color: #FF6D6D;
+  color: #FF6D6D;
 }
 
 .cv-header-days,
@@ -161,89 +214,107 @@ const onClickDate = (day) => {
 .cv-week,
 .cv-day,
 .cv-item {
-    border-style: solid;
-    border-color: #F9F9FF;
-    background-color: #F9F9FF;
-    font-size: 14px;
+  border-style: solid;
+  border-color: #F9F9FF;
+  background-color: #F9F9FF;
+  font-size: 14px;
 }
 
 .vc-header .cv-header-days {
-    background-color: #F9F9FF;
+  background-color: #F9F9FF;
 }
 
-.cv-day.good {
-    margin-top: 0.25rem;
-    background-color: #1a73e8;
-    color: #fff;
-}
-.cv-day.bad {
-    margin-top: 0.25rem;
-    background-color: #1a73e8;
-    color: #fff;
-}
 .theme-default .cv-item {
-    background-color: transparent;
-    border-color: transparent;
-    font-size: 14px;
-    text-align: center;
+  background-color: transparent;
+  border-color: transparent;
+  font-size: 12px;
+  text-align: center;
 }
+
 .theme-default .cv-day.past {
-    background-color: #F9F9FF;
+  background-color: #F9F9FF;
 }
 
 .cv-wrapper, .cv-wrapper div {
-    box-sizing: border-box;
-    line-height: 1em;
-    font-size: 20px;
-    text-align: center;
+  box-sizing: border-box;
+  line-height: 1em;
+  font-size: 16px;
+  text-align: center;
+  //margin-top: 5px;
 }
 
 .theme-gcal .cv-item {
-    border-color: transparent;
-    border-radius: 0.25em;
-    background-color: transparent; /* 배경 투명 */
-    padding-top: 0.325em;
-    padding-bottom: 0.325em;
-    padding-left: 0.6em;
+  border-color: transparent;
+  border-radius: 0.25em;
+  background-color: transparent;
+  padding-top: 0.325em;
+  padding-bottom: 0.325em;
+  padding-left: 0.6em;
 
-    /* Figma Css 추가 */
-    font-size: 10px;
+  font-size: 10px;
 }
 
 .cv-day {
-	display: flex;
+  display: flex;
 
-	/* Font 크기 설정 */
-	font-weight: 400;
-	font-size: 20px;
-	line-height: 24px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
 
-	/* Shorthand flex: 1 1 0 not supported by IE11 */
-	flex-grow: 1;
-	flex-shrink: 0;
-	flex-basis: 0;
-	position: relative; /* Fallback for IE11, which doesn't support sticky */
-	position: sticky; /* When week's items are scrolled, keep the day content fixed */
-	top: 0;
-	border-width: 1px 1px 0 0;
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 0;
+  //position: relative;
+  position: sticky;
+  top: 0;
+  border-width: 1px 1px 0 0;
+  direction: initial;
+  padding-right: 5px;
 
-	/* Restore user's direction setting (overridden for week) */
-	direction: initial;
-	
+  justify-content: center;
 }
 
 .cv-day-number {
-    font-size: 10px;
+  width: 0px;
+  height: 0px;
+
+  /* 캘린더에 기본으로 표시되는 day number를 hidden으로 변경 */
+  visibility: collapse;
+}
+
+.cv-day-good {
+  background-color: rgba(10, 136, 190, 0.27);
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  font-size: 10px;
+  padding-top: 5px;
+}
+
+.cv-day-bad {
+  background-color: rgba(247, 68, 71, 0.27);
+  font-size: 14px;
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  padding-top: 5px;
+}
+
+.cv-day-no-eval {
+  background-color: transparent;
+  font-size: 14px;
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
 }
 
 .cv-day.outsideOfMonth {
-    visibility: hidden; /* 요소는 숨기되 공간은 차지함 */
-    height: 0; /* 높이를 0으로 설정 */
-    overflow: hidden; /* 넘치는 내용은 숨김 */
+  visibility: hidden;
+  height: 0;
+  overflow: hidden;
 }
 
 .theme-default .cv-day.today {
-    background-color: #F9F9FF
+  background-color: #F9F9FF
 }
-
 </style>
--- src/components/calendar/CustomCalendarViewHeader.vue
+++ src/components/calendar/CustomCalendarViewHeader.vue
@@ -1,99 +1,144 @@
 <template>
-	<div class="cv-header">
-		<div class="cv-header-nav">
-			<button
-				:disabled="!headerProps.previousPeriod"
-				class="previousPeriod"
-				aria-label="Previous Period"
-				@click.prevent="onInput(headerProps.previousPeriod!)">
+  <div class="cv-header">
+    <div class="cv-header-nav">
+      <button
+          :disabled="!headerProps.previousPeriod"
+          class="previousPeriod"
+          aria-label="Previous Period"
+          @click.prevent="onInput(headerProps.previousPeriod!)">
         <img src="@/assets/icons/calendar-previous-month.svg" alt="Previous Month"/>
-			</button>
-		</div>
-		<div class="periodLabel">
-			<slot name="label">{{ headerProps.periodLabel }}</slot>
-		</div>
-		<div class="cv-header-nav">
-			<button
-				:disabled="!headerProps.nextPeriod"
-				class="nextPeriod"
-				aria-label="Next Period"
-				@click.prevent="onInput(headerProps.nextPeriod!)">
+      </button>
+    </div>
+    <div class="periodLabel" @click="handleClickPeriodLabel">
+      <slot name="label">{{ customPeriodLabel }}</slot>
+    </div>
+    <div class="cv-header-nav">
+      <button
+          :disabled="!headerProps.nextPeriod"
+          class="nextPeriod"
+          aria-label="Next Period"
+          @click.prevent="onInput(headerProps.nextPeriod!)">
         <img src="@/assets/icons/calendar-next-month.svg" alt="Next Month"/>
-			</button>
-		</div>
-	</div>
+      </button>
+    </div>
+  </div>
   <div class="triangle-button">
 
   </div>
 </template>
 <script setup lang="ts">
 import type { IHeaderProps } from "vue-simple-calendar/dist/src/IHeaderProps";
+import {defineProps, defineEmits, onMounted, ref, watch} from 'vue';
 
-defineProps({
-	headerProps: {
-		type: Object as () => IHeaderProps,
-		required: true,
-	},
-	previousYearLabel: { type: String, default: "<<" },
-	previousPeriodLabel: { type: String, default: "<" },
-	nextPeriodLabel: { type: String, default: ">" },
-	nextYearLabel: { type: String, default: ">>" },
+
+const props = defineProps({
+  headerProps: {
+    type: Object as () => IHeaderProps,
+    required: true,
+  },
+  previousYearLabel: { type: String, default: "<<" },
+  previousPeriodLabel: { type: String, default: "<" },
+  nextPeriodLabel: { type: String, default: ">" },
+  nextYearLabel: { type: String, default: ">>" },
 })
 
+const customPeriodLabel = ref('');
+const monthMapping: { [key: string]: string } = {
+  January: '1월',
+  February: '2월',
+  March: '3월',
+  April: '4월',
+  May: '5월',
+  June: '6월',
+  July: '7월',
+  August: '8월',
+  September: '9월',
+  October: '10월',
+  November: '11월',
+  December: '12월',
+};
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+// 월 표시 변경하는 메서드
+function changeMonth() {
+  // 월 변경 ('September 2024' -> '9월')
+  const periodLabel = props.headerProps.periodLabel;        // 'September 2024'
+  const [month, year] = periodLabel.split(' ');    // 'September'
+  customPeriodLabel.value = monthMapping[month] || month;   // '9월'
+
+  // console.log(props.headerProps)
+  return customPeriodLabel.value
+}
+
+// 월 클릭 시 가계부 메인 페이지(월별 전체 조회)로 이동
+const handleClickPeriodLabel = () => {
+  router.push('/account-book');
+}
+
+// headerProps가 변경될 때마다 changeMonth 호출
+watch(() => props.headerProps.periodLabel, () => {
+  changeMonth();
+});
+
 const emit = defineEmits<{
-	(e: "input", day: Date): void
+  (e: "input", day: Date): void
 }>()
 
+changeMonth();
+
 const onInput = (d: Date): void => emit("input", d)
+
 </script>
 <style>
 
 .cv-header {
-	display: ruby;
-	flex: 0 1 auto;
-	flex-flow: row nowrap;
-	align-items: center;
-	justify-content: space-between;
+  display: ruby;
+  flex: 0 1 auto;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
   gap: 5em;
-	min-height: 2.5em;
-	border-width: 1px 1px 0 1px;
-	text-align: center;
+  min-height: 2.5em;
+  border-width: 1px 1px 0 1px;
+  text-align: center;
   font-family: 'Noto Sans KR';
 }
 
 .cv-header .periodLabel {
-	display: flex;
-	flex: 1 1 auto;
-	flex-flow: row nowrap;
-	min-height: 1.5em;
-	line-height: 1;
-	align-items: center;
-	text-align: center;
-	justify-content: center;
+  display: flex;
+  flex: 1 1 auto;
+  flex-flow: row nowrap;
+  min-height: 1.5em;
+  line-height: 1;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
 }
 
 .cv-header,
 .cv-header button {
-	border-style: solid;
-	border-color: transparent;
+  border-style: solid;
+  border-color: transparent;
 }
 
 .cv-header-nav,
 .cv-header .periodLabel {
-	margin: 0.1em 0.6em;
+  margin: 0.1em 0.6em;
 }
 
 .cv-header-nav button,
 .cv-header .periodLabel {
-	padding: 0.4em 0.6em;
+  padding: 0.4em 0.6em;
 }
 
 .cv-header button {
-	box-sizing: border-box;
-	line-height: 1em;
-	font-size: 1em;
-	border-width: 1px;
-	background-color: #F9F9FF;
+  box-sizing: border-box;
+  line-height: 1em;
+  font-size: 1em;
+  border-width: 1px;
+  background-color: #F9F9FF;
 }
 
 .theme-default .cv-header, .theme-default .cv-header-day {
@@ -101,7 +146,7 @@ const onInput = (d: Date): void => emit("input", d)
 }
 
 .theme-default .cv-header, .theme-default .cv-header-day {
-  font-size: 16px;
+  font-size: 12px;
   padding: 10px;
 }
 
